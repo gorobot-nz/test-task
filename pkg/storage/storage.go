0f3fcae2e7a6d5ef28ab4f5271d6d35adfe4a068
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2024-01-14 18:59:11 +0300

    Add mutex to storage

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,25 +1,33 @@
 package storage
 
-import "slices"
+import (
+	"slices"
+	"sync"
+)
 
 type Storage[V any] struct {
+	m sync.Mutex
+
 	storageMap map[string]V
 	keysSlice  []string
 }
 
 func NewStorage[V any]() *Storage[V] {
 	return &Storage[V]{
+		m:          sync.Mutex{},
 		storageMap: make(map[string]V),
 		keysSlice:  make([]string, 0, 1024),
 	}
 }
 
 func (s *Storage[V]) Set(key string, value V) {
+	s.m.Lock()
 	s.storageMap[key] = value
 
 	if !slices.Contains(s.keysSlice, key) {
 		s.keysSlice = append(s.keysSlice, key)
 	}
+	s.m.Unlock()
 }
 
 func (s *Storage[V]) Get(key string) V {
@@ -27,19 +35,23 @@ func (s *Storage[V]) Get(key string) V {
 }
 
 func (s *Storage[V]) List() []V {
+	s.m.Lock()
 	var result = make([]V, len(s.keysSlice))
 
 	for index, val := range s.keysSlice {
 		result[index] = s.storageMap[val]
 	}
+	s.m.Unlock()
 
 	return result
 }
 
 func (s *Storage[V]) Delete(key string) {
+	s.m.Lock()
 	index := slices.Index(s.keysSlice, key)
 	if index != -1 {
 		slices.Delete(s.keysSlice, index, index+1)
 		delete(s.storageMap, key)
 	}
+	s.m.Unlock()
 }
