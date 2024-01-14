9c0af9d29f85aca942bbb7fb318d3decfa2624e2
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2024-01-14 17:38:12 +0300

    Add unimplemented service and repository

---
+++ internal/repository/users/storage_repository.go
@@ -0,0 +1,45 @@
+package users
+
+import (
+	"context"
+	userv1 "github.com/gorobot-nz/test-task/gen/proto/user/v1"
+	"github.com/gorobot-nz/test-task/pkg/storage"
+)
+
+type StorageRepository struct {
+	storage *storage.Storage[*userv1.User]
+}
+
+func NewStorageRepository() *StorageRepository {
+	return &StorageRepository{storage: storage.NewStorage[*userv1.User]()}
+}
+
+func (s *StorageRepository) NewUser(ctx context.Context, user *userv1.User) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *StorageRepository) GetUsers(ctx context.Context, page, limit int32) ([]*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *StorageRepository) GetUserById(ctx context.Context, id string) (*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *StorageRepository) GetUserByUsername(ctx context.Context, username string) (*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *StorageRepository) UpdateUser(ctx context.Context, user *userv1.User) (*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *StorageRepository) DeleteUser(ctx context.Context, id string) error {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ internal/service/users/service.go
@@ -0,0 +1,55 @@
+package users
+
+import (
+	"context"
+	userv1 "github.com/gorobot-nz/test-task/gen/proto/user/v1"
+)
+
+type Repository interface {
+	NewUser(ctx context.Context, user *userv1.User) (string, error)
+	GetUsers(ctx context.Context, page, limit int32) ([]*userv1.User, error)
+	GetUserById(ctx context.Context, id string) (*userv1.User, error)
+	GetUserByUsername(ctx context.Context, username string) (*userv1.User, error)
+	UpdateUser(ctx context.Context, user *userv1.User) (*userv1.User, error)
+	DeleteUser(ctx context.Context, id string) error
+}
+
+type Service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *Service {
+	return &Service{
+		repository: repository,
+	}
+}
+
+func (s *Service) NewUser(ctx context.Context, user *userv1.User) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Service) GetUsers(ctx context.Context, page, limit int32) ([]*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Service) GetUserById(ctx context.Context, id string) (*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Service) GetUserByUsername(ctx context.Context, username string) (*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Service) UpdateUser(ctx context.Context, user *userv1.User) (*userv1.User, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *Service) DeleteUser(ctx context.Context, id string) error {
+	//TODO implement me
+	panic("implement me")
+}
